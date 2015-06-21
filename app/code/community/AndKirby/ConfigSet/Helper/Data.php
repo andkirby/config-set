f35eef38b7206e4b05da60d0e6041c3d1e2d75d1
Andrew Roslik <andkirby@gmail.com>
2015-06-21 21:46:19 +0300

    - Added an ability to show xpath of config nodes.

--- app/code/community/AndKirby/ConfigSet/Helper/Data.php
+++ app/code/community/AndKirby/ConfigSet/Helper/Data.php
@@ -6,4 +6,13 @@
  */
 class AndKirby_ConfigSet_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * Get flag of showing system config xpath to values
+     *
+     * @return bool
+     */
+    public function isShowSystemConfigPath()
+    {
+        return (bool)Mage::getSingleton('adminhtml/session')->getShowSystemConfigPath();
+    }
 }
